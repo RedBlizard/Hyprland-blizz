3160e9d159d955eb6ab07a69b1343fed2173dd45
RedBlizard <grote2207@gmail.com>
2024-05-06 19:00:20 +0200

    standard audio volume set to 35% now

--- .config/hypr/scripts/volume.sh
+++ .config/hypr/scripts/volume.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Set the default volume to 35%
+pactl set-sink-volume @DEFAULT_AUDIO_SINK@ 35%
+
 # Function to display volume change notification
 show_volume_notification() {
     local icon=$1
     local volume_bar=$2
     local vol=$3
-    dunstify -a "Volume" -h string:x-dunst-stack-tag:volume "$icon  $volume_bar : $vol%" &
+    dunstify -a "Volume" -h string:x-dunst-stack-tag:volume -i "$icon" -u low "$volume_bar : $vol%" &
 }
 
 # Function to display microphone volume change notification
@@ -13,67 +16,61 @@ show_mic_notification() {
     local icon=$1
     local volume_bar=$2
     local vol=$3
-    dunstify -a "Microphone volume" -h string:x-dunst-stack-tag:mic "$icon  $volume_bar : $vol%" &
+    dunstify -a "Microphone volume" -h string:x-dunst-stack-tag:mic -i "$icon" -u low "$volume_bar : $vol%" &
 }
 
 # Function for Volume Up Script
 volume_up() {
     # Lock file
     local lockfile="/tmp/volume_lock"
-    touch $lockfile
+    touch "$lockfile"
     
     # Define bar and space variables
     bar="█" # You can use any character you like to represent the volume level
     space="░" # You can use any character you like to represent the remaining space
 
-    # Decrease volume by 5%
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+    # Increase volume by 5%
+    pactl set-sink-volume @DEFAULT_AUDIO_SINK@ +5%
 
     # Get volume level for the default audio sink
-    vol=$(wpctl get-volume "@DEFAULT_AUDIO_SINK@")
-    vol=${vol#* }
-    vol=$(echo "$vol" | tr -d ".")
-    while [[ ${vol:0:1} == "0" ]]; do vol=${vol:1}; done
-
+    vol=$(pamixer --get-volume)
+    
     # Generate volume bar
     volume_bar=$(seq -s "$bar" 0 $(((vol - 1) / 5)) | sed 's/[0-9]//g')
     remaining_space=$(seq -s "$space" 0 $((20 - vol / 5)) | sed 's/[0-9]//g')
 
-    # Display notification with volume level and icon for volume decrease
-    dunstify -a "Volume up" "  $volume_bar$remaining_space : $(printf "%s%%" "$vol")"
+    # Display notification with volume level and icon for volume increase
+    show_volume_notification "audio-volume-high" "$volume_bar" "$vol"
 
     # Remove lock file
-    rm $lockfile
+    rm "$lockfile"
 }
 
 # Function for Volume Down Script
 volume_down() {
     # Lock file
     local lockfile="/tmp/volume_lock"
-    touch $lockfile
+    touch "$lockfile"
     
     # Define bar and space variables
     bar="█" # You can use any character you like to represent the volume level
     space="░" # You can use any character you like to represent the remaining space
 
     # Decrease volume by 5%
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+    pactl set-sink-volume @DEFAULT_AUDIO_SINK@ -5%
 
     # Get volume level for the default audio sink
-    vol=$(wpctl get-volume "@DEFAULT_AUDIO_SINK@")
-    vol=${vol#* }
-    vol=$(echo "$vol" | tr -d ".")
-    while [[ ${vol:0:1} == "0" ]]; do vol=${vol:1}; done
-
+    vol=$(pamixer --get-volume)
+    
     # Generate volume bar
     volume_bar=$(seq -s "$bar" 0 $(((vol - 1) / 5)) | sed 's/[0-9]//g')
     remaining_space=$(seq -s "$space" 0 $((20 - vol / 5)) | sed 's/[0-9]//g')
 
     # Display notification with volume level and icon for volume decrease
-    dunstify -a "Volume down" "  $volume_bar$remaining_space : $(printf "%s%%" "$vol")"
+    show_volume_notification "audio-volume-high" "$volume_bar" "$vol"
 
     # Remove lock file
-    rm $lockfile
+    rm "$lockfile"
 }
 
 # Function for Volume Mute Script
@@ -95,80 +92,73 @@ volume_mute() {
     if [ "$current_mute" == "yes" ]; then
         pactl set-sink-mute "$active_sink" 0  # Unmute
         echo "Unmuted $active_sink"
-        message="Volume unmuted "
+        message="Volume unmuted"
     else
         pactl set-sink-mute "$active_sink" 1  # Mute
         echo "Muted $active_sink"
         icon="audio-volume-muted-symbolic"
-        message="Volume muted "
+        message="Volume muted"
     fi
 
     # Display notification
-    dunstify -a "Volume Control" -i "$icon" "$message"
+    show_volume_notification "$icon" "" "$message"
 
     # Remove lock file
     rm "$lockfile"
 }
 
-
-# Function for Volume Up Script
+# Function for Microphone Volume Increase Script
 mic_volume_up() {
     # Lock file
-    local lockfile="/tmp/volume_lock"
-    touch $lockfile
+    local lockfile="/tmp/mic_lock"
+    touch "$lockfile"
     
     # Define bar and space variables
     bar="█" # You can use any character you like to represent the volume level
     space="░" # You can use any character you like to represent the remaining space
 
-    # Decrease volume by 5%
-    wpctl set-volume @DEFAULT_SOURCE@ 5%+
+    # Increase microphone volume by 5%
+    pactl set-source-volume @DEFAULT_SOURCE@ +5%
 
     # Get volume level for the default audio sink
-    vol=$(wpctl get-volume "@DEFAULT_SOURCE@")
-    vol=${vol#* }
-    vol=$(echo "$vol" | tr -d ".")
-    while [[ ${vol:0:1} == "0" ]]; do vol=${vol:1}; done
-
+    vol=$(pamixer --get-volume)
+    
     # Generate volume bar
     volume_bar=$(seq -s "$bar" 0 $(((vol - 1) / 5)) | sed 's/[0-9]//g')
     remaining_space=$(seq -s "$space" 0 $((20 - vol / 5)) | sed 's/[0-9]//g')
 
-    # Display notification with volume level and icon for volume decrease
-    dunstify -a "Microphone volume up" "  $volume_bar$remaining_space : $(printf "%s%%" "$vol")"
+    # Display notification with volume level and icon for microphone volume increase
+    show_mic_notification "microphone-sensitivity-high" "$volume_bar" "$vol"
 
     # Remove lock file
-    rm $lockfile
+    rm "$lockfile"
 }
 
 # Function for Microphone Volume Decrease Script
 mic_volume_down() {
     # Lock file
     local lockfile="/tmp/mic_lock"
-    touch $lockfile
+    touch "$lockfile"
     
     # Define bar and space variables
     bar="█" # You can use any character you like to represent the volume level
     space="░" # You can use any character you like to represent the remaining space
 
-    # Decrease volume by 5%
-    wpctl set-volume @DEFAULT_SOURCE@ 5%-
+    # Decrease microphone volume by 5%
+    pactl set-source-volume @DEFAULT_SOURCE@ -5%
 
     # Get volume level for the default audio sink
-    vol=$(wpctl get-volume "@DEFAULT_SOURCE@")
-    vol=${vol#* }
-    vol=$(echo "$vol" | tr -d ".")
-    while [[ ${vol:0:1} == "0" ]]; do vol=${vol:1}; done
-
+    vol=$(pamixer --get-volume)
+    
     # Generate volume bar
     volume_bar=$(seq -s "$bar" 0 $(((vol - 1) / 5)) | sed 's/[0-9]//g')
     remaining_space=$(seq -s "$space" 0 $((20 - vol / 5)) | sed 's/[0-9]//g')
 
-    # Display notification with volume level and icon for volume decrease
-    dunstify -a "Microphone volume down" "  $volume_bar$remaining_space : $(printf "%s%%" "$vol")"
+    # Display notification with volume level and icon for microphone volume decrease
+    show_mic_notification "microphone-sensitivity-high" "$volume_bar" "$vol"
 
     # Remove lock file
-    rm $lockfile
+    rm "$lockfile"
 }
 
 # Function for Microphone Mute Script
@@ -190,38 +180,38 @@ mic_mute() {
     if [ "$current_mute" == "yes" ]; then
         pactl set-source-mute "$active_source" 0  # Unmute
         echo "Microphone unmuted: $active_source"
-        icon="microphone-sensitivity-muted"  # Unmuted icon
-        message="Microphone unmuted "
+        icon="microphone-sensitivity-high"  # Unmuted icon
+        message="Microphone unmuted"
     else
         pactl set-source-mute "$active_source" 1  # Mute
         echo "Microphone muted: $active_source"
-        icon="microphone-sensitivity-high"  # Muted icon
-        message="Microphone muted "
+        icon="microphone-sensitivity-muted"  # Muted icon
+        message="Microphone muted"
     fi
 
     # Display notification
-    dunstify -a "Microphone control" -i "$icon" "$message"
+    show_mic_notification "$icon" "" "$message"
 
     # Remove lock file
     rm "$lockfile"
 }
 
 # Main script starts here
-echo "Starting Combined Script"
+echo "Starting Volume Script"
 
-# Check if increasing regular volume
+# Check which action to perform
 if [[ $1 == "volume_up" ]]; then
     volume_up
 elif [[ $1 == "volume_down" ]]; then
     volume_down
 elif [[ $1 == "volume_mute" ]]; then
-    volume_mute                
+    volume_mute
 elif [[ $1 == "mic_up" ]]; then
     mic_volume_up
 elif [[ $1 == "mic_down" ]]; then
     mic_volume_down
 elif [[ $1 == "mic_mute" ]]; then
-    mic_mute    
+    mic_mute
 fi
 
-echo "Combined Script Finished"
+echo "Volume Script Finished"
