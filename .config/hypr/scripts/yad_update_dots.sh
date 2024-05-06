909c5c172f43e55a18a5aafa82d6033e5663fb92
RedBlizard <grote2207@gmail.com>
2024-05-06 17:45:06 +0200

    add creation symlinks back in to the script

--- .config/hypr/scripts/yad_update_dots.sh
+++ .config/hypr/scripts/yad_update_dots.sh
@@ -227,5 +227,13 @@ rm -rf $HOME/README.md
 rm -rf $HOME/sddm-images
 rm -rf $HOME/LICENSE
 
+# Change to the scripts directory
+cd "$HOME/.config/hypr/scripts" || { echo "Failed to change to the scripts directory." >&2; exit 1; }
+
+# Create symlinks in /usr/bin for easy access, suppress output and errors
+sudo ln -sf "$(pwd)/hypr_check_updates.sh" /usr/bin/hypr_check_updates >/dev/null 2>&1
+sudo ln -sf "$(pwd)/hypr-welcome" /usr/bin/hypr-welcome >/dev/null 2>&1
+sudo ln -sf "$(pwd)/hypr-eos-kill-yad-zombies" /usr/bin/hypr-eos-kill-yad-zombies >/dev/null 2>&1
+
 # Notify user about the end of the script
 notify-send "We are done enjoy your updated Hyprland experience"
