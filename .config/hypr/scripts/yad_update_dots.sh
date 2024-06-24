e72b9676cc7a2c0ab44d070e5f1995506f1f5087
RedBlizard <grote2207@gmail.com>
2024-06-24 03:11:41 +0200

    trying different gtk theme fix

--- .config/hypr/scripts/yad_update_dots.sh
+++ .config/hypr/scripts/yad_update_dots.sh
@@ -304,5 +304,20 @@ if ! check_symlinks; then
     sudo ln -sf "$update_script" "$symlink"
 fi
 
+# Set GNOME desktop interface settings using gsettings
+# Gtk Theme workaround
+gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Frappe-Standard-Blue-Dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 10'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 10'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 12'
+gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-dark'
+gsettings set org.gnome.desktop.interface cursor-size '24'
+gsettings set org.gnome.desktop.interface font-antialiasing 'rgba'
+gsettings set org.gnome.desktop.interface font-hinting 'full'
+
+
+# End of script
+
 # Notify user about the end of the script
 notify-send "We are done enjoy your updated Hyprland experience"
