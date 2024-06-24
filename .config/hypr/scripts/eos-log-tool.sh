59bf9a43c1ea954ab2ceccadcac0151b8e30bc75
RedBlizard <grote2207@gmail.com>
2024-06-24 03:38:34 +0200

    small change to this script gtk theme added

--- .config/hypr/scripts/eos-log-tool.sh
+++ .config/hypr/scripts/eos-log-tool.sh
@@ -3,6 +3,10 @@
 # kill hypr-welcome
 pkill -f yad
 
+# Set default GTK theme
+default_theme='Catppuccino-Frappe-Standard-Blue-Dark'
+gsettings set org.gnome.desktop.interface gtk-theme "$default_theme"
+
 # Run eos-rankmirrors
 eos-log-tool
 
