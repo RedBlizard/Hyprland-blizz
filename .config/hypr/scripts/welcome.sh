f0981d921138f7461445363c8c25ca59be29f9d1
RedBlizard <grote2207@gmail.com>
2024-06-27 19:59:02 +0200

    Commit rollback welcome.sh

--- .config/hypr/scripts/welcome.sh
+++ .config/hypr/scripts/welcome.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# creates flag for the monitor_workspaces_configurator script
+
+# Define the lock file
+lock_file="$HOME/.config/hypr/scripts/monitor_workspaces_flag"
+
+# Check if the lock file exists
+if [ -e "$lock_file" ]; then
+    echo "monitor_workspaces_configurator has already run. Exiting."
+    exit 1
+fi
+# Your script's code goes here
+echo "Starting monitor_workspaces_configurator script..."
+# This is where you would put the actual commands you want your script to run
+# Example:
+# ./path/to/yad_switch-waybar-config.sh
+$HOME/.config/hypr/scripts/monitor_workspaces_configurator.sh
+
+# Create the lock file
+echo "Creating flag file..."
+touch "$lock_file"
+
+echo "monitor_workspaces_configurator script completed successfully."
+
 # creates flag for the yad_switch-waybar-config script
 
 sleep 2
