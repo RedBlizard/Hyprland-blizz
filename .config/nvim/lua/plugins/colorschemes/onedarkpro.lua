325921fd4b2fcb8305435c8cb6eb3d6d05d95fb8
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2024-10-20 15:14:00 +0200

    Delete .config/nvim directory

--- .config/nvim/lua/plugins/colorschemes/onedarkpro.lua
+++
@@ -1,6 +0,0 @@
-local status, plugin = pcall(require,'onedarkpro')
-if not status then
-    print('Plugin Error: ', plugin)
-    return
-end
-plugin.setup({})
--- .config/nvim/lua/plugins/indent.lua
+++
@@ -1,8 +0,0 @@
-local status, plugin = pcall(require,'ibl')
-if not status then
-    print('Error with plugin: ', plugin)
-    return
-end
-plugin.setup{}
-
-
