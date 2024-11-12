901f06d7a0d589ffb4f463c9a8666ab32a0a950b
RedBlizard <grote2207@gmail.com>
2024-11-12 02:44:35 +0100

    commit of big script change

--- .config/hypr/scripts/watch-hyprland-config.sh
+++ .config/hypr/scripts/watch-hyprland-config.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/usr/bin/fish
 
-# Install inotify-tools if you haven't
-#  sudo pacman -S inotify-tools
+set config_dir $HOME/.config/hypr/conf
+set hyprland_config $HOME/.config/hypr/hyprland.conf
 
-# Create a script to watch for changes in sourced files
-while inotifywait -e modify ~/.config/hypr/conf/*.conf; do
-    hyprctl reload
-done
+echo "Script started" >> $HOME/.config/hypr/scripts/hypr-reload.log 2>&1
+
+# Find all .conf files in the config directory and include hyprland.conf
+set conf_files (find $config_dir -type f -name "*.conf")
+set all_files $conf_files $hyprland_config
+
+# Log which files are being watched
+echo "Watching: $all_files" >> $HOME/.config/hypr/scripts/hypr-reload.log 2>&1
+
+while true
+    # Use inotifywait with the found .conf files including hyprland.conf
+    inotifywait -e modify $all_files
+    echo "Change detected! Attempting to reload Hyprland..." >> $HOME/.config/hypr/scripts/hypr-reload.log 2>&1
+    /usr/bin/hyprctl reload >> $HOME/.config/hypr/scripts/hypr-reload.log 2>&1
+end
