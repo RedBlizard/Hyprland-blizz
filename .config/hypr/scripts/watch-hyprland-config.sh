c94928c03851ad00976e7e865a661b6f03a5f616
RedBlizard <grote2207@gmail.com>
2024-11-12 01:16:55 +0100

    commit of small fix

--- .config/hypr/scripts/watch-hyprland-config.sh
+++ .config/hypr/scripts/watch-hyprland-config.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
+
 # Install inotify-tools if you haven't
-sudo pacman -S inotify-tools
+#  sudo pacman -S inotify-tools
 
 # Create a script to watch for changes in sourced files
 while inotifywait -e modify ~/.config/hypr/conf/*.conf; do
     hyprctl reload
 done
-
