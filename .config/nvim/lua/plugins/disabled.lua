f926882e4a0eb5422bdd311cbb1545327f02f68a
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2024-12-22 22:57:12 +0100

    Update disabled.lua

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false }, -- Not necessary (Harpoon!)
   { "nvim-neo-tree/neo-tree.nvim", enabled = false }, -- Switched to Oil
   { "ThePrimeagen/vim-be-good", enabled = false }, -- Just don't really need it
+  { "nvim-neodev/neodev.nvim", enabled = false }, -- no longer needed
 }
