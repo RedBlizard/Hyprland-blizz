7bc07ee81150b2a684a4c0426076a22d73b2c5a4
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2024-12-30 16:44:54 +0100

    Update input.sh
    script updated to better version.

--- .config/hypr/hyprlock-scripts/input.sh
+++ .config/hypr/hyprlock-scripts/input.sh
@@ -1,8 +1,26 @@
-input=$(ibus engine | grep BambooUs)
+#!/bin/bash
 
-if [ -n "$input" ]
-then
-   echo "US ⠀⠀"
-else
-   echo "US ⠀⠀"
-fi
+# Extract all active keymap entries
+layout=$(hyprctl devices | grep "active keymap:" | awk '{print $3}' | tr -d '()' | uniq)
+
+# Map the layout to language labels
+case "$layout" in
+    "English")
+        echo "US ⠀⠀"
+        ;;
+    "Dutch")
+        echo "NL ⠀⠀"
+        ;;
+    "Spanish")
+        echo "ES ⠀⠀"
+        ;;
+    "German")
+        echo "DE ⠀⠀"
+        ;;
+    "Russian")
+        echo "RU ⠀⠀"
+        ;;
+    *)
+        echo "Unknown ⠀⠀"
+        ;;
+esac
