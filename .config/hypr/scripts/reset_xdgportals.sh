5b58920d639e8fa9e6897e99e4b368926bbbb7b2
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2025-07-31 18:03:36 +0200

    Update reset_xdgportals.sh
    script fix for xdg portals.

--- .config/hypr/scripts/reset_xdgportals.sh
+++ .config/hypr/scripts/reset_xdgportals.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
-# __  ______   ____ 
+# __  ______   ____
 # \ \/ /  _ \ / ___|
-#  \  /| | | | |  _ 
+#  \  /| | | | |  _
 #  /  \| |_| | |_| |
 # /_/\_\____/ \____|
-#                   
-# ----------------------------------------------------- 
-sleep 1
+#
 
-# kill all possible running xdg-desktop-portals
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-kde
-killall xdg-desktop-portal-lxqt
-killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gtk
-killall xdg-desktop-portal
-sleep 1
+# Setup Timers
+_sleep1="0.1"
+_sleep2="0.5"
+_sleep3="2"
+_sleep4="1"
 
-# start xdg-desktop-portal-hyprland
+sleep $_sleep4
+
+# Kill all possible running xdg-desktop-portals
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-kde
+killall -e xdg-desktop-portal-lxqt
+killall -e xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal
+
+# Set required environment variables
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
+
+# Stop all services
+systemctl --user stop pipewire
+systemctl --user stop wireplumber
+systemctl --user stop xdg-desktop-portal
+systemctl --user stop xdg-desktop-portal-gnome
+systemctl --user stop xdg-desktop-portal-kde
+systemctl --user stop xdg-desktop-portal-wlr
+systemctl --user stop xdg-desktop-portal-hyprland
+sleep $_sleep1
+
+# Start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
+sleep $_sleep3
 
-# start xdg-desktop-portal
+# Start xdg-desktop-portal-gtk
+if [ -f /usr/lib/xdg-desktop-portal-gtk ]; then
+    /usr/lib/xdg-desktop-portal-gtk &
+    sleep $_sleep1
+fi
+
+# Start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
-sleep 1
+sleep $_sleep2
+
+# Start required services
+systemctl --user start pipewire
+systemctl --user start wireplumber
+systemctl --user start xdg-desktop-portal
+systemctl --user start xdg-desktop-portal-hyprland
+
+
