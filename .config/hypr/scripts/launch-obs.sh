654333ccd3b226ea5e9957a24d1e0d75d36b7a8f
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2026-05-21 20:27:22 +0200

    Delete .config/hypr directory

--- .config/hypr/scripts/launch-obs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Log the date and time of script execution
-echo "Launching OBS Studio at $(date)" >> /tmp/obs-launch-log.txt
-
-# Launch OBS Studio
-obs &>> /tmp/obs-launch-log.txt
-
